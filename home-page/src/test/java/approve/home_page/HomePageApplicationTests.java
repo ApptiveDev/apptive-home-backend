bd4584b65133823c038fe57e906d1c18118e9a51
JunHyeong-99 <64734115+JunHyeong-99@users.noreply.github.com>
2024-09-07 16:44:28 +0900

    init project

---
+++ home-page/src/test/java/approve/home_page/HomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package approve.home_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
